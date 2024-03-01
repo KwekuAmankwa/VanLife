2a3f22ce9a8eac37e7ebab6d81cedeb7fe1bfce1
Oak <mickyodum@gmail.com>
2024-03-01 04:12:36 +0100

    navbar and layout component

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,22 @@ import React from 'react'
 import {BrowserRouter, Routes, Route, Link} from "react-router-dom"
 import Home from './assets/home'
 import About from './assets/about'
-import logo from "/logog.png"
 import Vans from './assets/vans'
 import VanDetails from './assets/vandetails'
 import "./server"
+import Layout from './components/layout'
 
 
 function App() {
   return (
     <BrowserRouter>
-      <header className="header">   
-        <Link to="/"><img src={logo} alt="vanlife-logo" className="logo"/></Link>
-        <nav className="navbar">
-          <Link to= "/about">About</Link>
-          <Link to= "/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path= "/" element={<Home/>} />
-        <Route path= "/about" element={<About/>} />
-        <Route path= "/vans" element={<Vans/>} />
-        <Route path= "/vans/:id" element={<VanDetails/>} />
+        <Route element = {<Layout />} >
+          <Route path= "/" element={<Home/>} />
+          <Route path= "/about" element={<About/>} />
+          <Route path= "/vans" element={<Vans/>} />
+          <Route path= "/vans/:id" element={<VanDetails/>} />
+        </Route>
       </Routes>
       <footer>Ⓒ 2022 #VANLIFE</footer>
     </BrowserRouter>
---
+++ src/components/header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "/logog.png"
+
+export default function Header (){
+    return (
+        <header className="header">   
+            <Link to="/"><img src={logo} alt="vanlife-logo" className="logo"/></Link>
+            <nav className="navbar">
+            <Link to= "/about">About</Link>
+            <Link to= "/vans">Vans</Link>
+            </nav>
+        </header>
+    )
+}
