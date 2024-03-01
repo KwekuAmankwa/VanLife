1ecb03dbfc2780439cd60111161d6685be66e74d
Oak <mickyodum@gmail.com>
2024-03-01 19:04:02 +0100

    specify and style active links

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import logo from "/logog.png"
 
 export default function Header (){
@@ -7,9 +7,26 @@ export default function Header (){
         <header className="header">   
             <Link to="/"><img src={logo} alt="vanlife-logo" className="logo"/></Link>
             <nav className="navbar">
-            <Link to="/host">Host</Link>
-            <Link to= "/about">About</Link>
-            <Link to= "/vans">Vans</Link>
+                <NavLink 
+                to="/host"
+                className={({isActive}) => isActive ? "active-link" : ""}
+                >
+                    Host
+                </NavLink>
+
+                <NavLink 
+                to= "/about"
+                className={({isActive}) => isActive ? "active-link" : ""}
+                >
+                    About           
+                </NavLink>
+
+                <NavLink 
+                to= "/vans"
+                className={({isActive}) => isActive ? "active-link" : ""}
+                >
+                    Vans
+                </NavLink>
             </nav>
         </header>
     )
