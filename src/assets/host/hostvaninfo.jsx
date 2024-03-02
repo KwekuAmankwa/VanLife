996a5ae20fde40582dbcd0a377587167b451d17d
Oak <mickyodum@gmail.com>
2024-03-02 04:18:43 +0100

    setting up outlet context

--- src/assets/host/hostvaninfo.jsx
+++ src/assets/host/hostvaninfo.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useOutletContext } from "react-router-dom";
 
 export default function HostVanInfo(){
+    const { van } = useOutletContext
     return(
         <h1>Info</h1>
     )
