9546e5910668877cf0e0eac676db37372f78da9e
Oak <mickyodum@gmail.com>
2024-03-04 04:00:25 +0100

    fixed 'to' attribute and added login navlink

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import logo from "/logog.png"
+import icon from "/user-icon.png"
 
 export default function Header (){
     return (
@@ -8,24 +9,30 @@ export default function Header (){
             <Link to="/"><img src={logo} alt="vanlife-logo" className="logo"/></Link>
             <nav className="navbar">
                 <NavLink 
-                to="/host"
+                to="host"
                 className={({isActive}) => isActive ? "active-link" : ""}
                 >
                     Host
                 </NavLink>
 
                 <NavLink 
-                to= "/about"
+                to= "about"
                 className={({isActive}) => isActive ? "active-link" : ""}
                 >
                     About           
                 </NavLink>
 
                 <NavLink 
-                to= "/vans"
+                to= "vans"
                 className={({isActive}) => isActive ? "active-link" : ""}
                 >
                     Vans
+                </NavLink>
+                <NavLink 
+                to= "login"
+                className="login-link"
+                >
+                    <img src={icon} alt="" className="login-icon" />
                 </NavLink>
             </nav>
         </header>
