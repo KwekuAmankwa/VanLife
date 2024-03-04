a7662f4d1d87f465e64ac3fdbb1645a325900bde
Oak <mickyodum@gmail.com>
2024-03-04 04:42:06 +0100

    using loader function instead of useState, useEffect, useParams to get vans from api

--- src/assets/host/hostvandetail.jsx
+++ src/assets/host/hostvandetail.jsx
@@ -1,18 +1,16 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useLoaderData} from "react-router-dom";
 import { Link, NavLink, Outlet } from "react-router-dom";
+import { getVans } from "../../api";
+
+
+export function loader({params}){
+    return getVans(params.id)
+}
 
 export default function HostVanDetail(){
 
-    const params = useParams()
-    const [van, setVan] = useState(null)
-
-    useEffect(() => {
-        fetch(`/api/vans/${params.id}`)
-        .then(res => res.json())
-        .then(data => setVan(data.vans))
-    },[params.id])
+    const van = useLoaderData()
 
     const activeStyles = {
         fontWeight: "bold",
@@ -23,54 +21,48 @@ export default function HostVanDetail(){
 
     return (
         <>
-            { van ? 
-                (
-                    <section className="host-van-index">
-                        <Link 
-                            to=".." 
-                            relative="path" 
-                            className="back-button">
-                            &larr; <span>Back to all vans</span>
-                        </Link>
-                        <div className="host-van-index-wrapper">
-                            <div className="host-van-initial">
-                                <div className="host-van-index-image">
-                                    <img src= {van.imageUrl} alt="" />    
-                                </div>
-                                <div className="host-van-index-details">
-                                    <i className={`van-type ${van.type} selected`}>{van.type}</i>
-                                    <p className="host-van-index-name">{van.name}</p>
-                                    <p className="host-van-index-price">${van.price} <span>/day</span> </p>
-                                </div>
-                            </div>
-                            <nav className="host-nav-layout">
-                                <NavLink
-                                    to= "."
-                                    end 
-                                    style = {({isActive}) => isActive ? activeStyles : null}
-                                    >
-                                        Details
-                                </NavLink>
-                                <NavLink
-                                    to= "pricing"
-                                    style = {({isActive}) => isActive ? activeStyles : null}
-                                    >
-                                        Pricing
-                                </NavLink>
-                                <NavLink
-                                    to= "photos"
-                                    style = {({isActive}) => isActive ? activeStyles : null}
-                                    >
-                                        Photos
-                                </NavLink>
-                            </nav>
-                            <Outlet context= {{ van }}/>
+            <section className="host-van-index">
+                <Link 
+                    to=".." 
+                    relative="path" 
+                    className="back-button">
+                    &larr; <span>Back to all vans</span>
+                </Link>
+                <div className="host-van-index-wrapper">
+                    <div className="host-van-initial">
+                        <div className="host-van-index-image">
+                            <img src= {van.imageUrl} alt="" />    
                         </div>
-                    </section>
-                    
-                ) 
-            : 
-                (<h2>Loading...</h2>  )}
+                        <div className="host-van-index-details">
+                            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                            <p className="host-van-index-name">{van.name}</p>
+                            <p className="host-van-index-price">${van.price} <span>/day</span> </p>
+                        </div>
+                    </div>
+                    <nav className="host-nav-layout">
+                        <NavLink
+                            to= "."
+                            end 
+                            style = {({isActive}) => isActive ? activeStyles : null}
+                            >
+                                Details
+                        </NavLink>
+                        <NavLink
+                            to= "pricing"
+                            style = {({isActive}) => isActive ? activeStyles : null}
+                            >
+                                Pricing
+                        </NavLink>
+                        <NavLink
+                            to= "photos"
+                            style = {({isActive}) => isActive ? activeStyles : null}
+                            >
+                                Photos
+                        </NavLink>
+                    </nav>
+                    <Outlet context= {{ van }}/>
+                </div>
+            </section>
         </>
     )
 }
