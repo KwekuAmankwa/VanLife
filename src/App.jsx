efc404ff81a08010d52f32818073cb137207e7ad
Oak <mickyodum@gmail.com>
2024-03-13 08:39:34 +0100

    changed loaderfunction names

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,21 @@ import React from 'react'
 import {RouterProvider, createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom"
 import Home from './assets/home'
 import About from './assets/about'
-import Vans, {loader as vansLoader} from './assets/vans/vans'
-import VanDetails, {loader as vanDetailsLoader} from './assets/vans/vandetails'
-import "./server"
+import Vans, {loaderFunction as vansLoader} from './assets/vans/vans'
+import VanDetails, {loaderFunction as vanDetailsLoader} from './assets/vans/vandetails'
 import Layout from './components/layout'
-import Dashboard, {loader as dashboardLoader} from './assets/host/dashboard'
+import Dashboard, {loaderFunction as dashboardLoader} from './assets/host/dashboard'
 import Income from './assets/host/income'
 import Reviews from './assets/host/reviews'
 import HostLayout from './components/hostlayout'
-import HostVans, {loader as hostVansLoader} from './assets/host/hostvans'
-import HostVanDetail, {loader as hostVanDetailLoader} from './assets/host/hostvandetail'
+import HostVans, {loaderFunction as hostVansLoader} from './assets/host/hostvans'
+import HostVanDetail, {loaderFunction as hostVanDetailLoader} from './assets/host/hostvandetail'
 import HostVanInfo from './assets/host/hostvaninfo'
 import HostVanPricing from './assets/host/hostvanpricing'
 import HostVanPhotos from './assets/host/hostvanphotos'
 import NotFound from './assets/notfound'
 import Error from './components/error'
-import Login, {loader as loginLoader, action as loginAction} from './assets/login'
+import Login, {loaderFunction as loginLoader, actionFunction as loginAction} from './assets/login'
 import { requireAuth } from './utilities'
 
 // 
@@ -41,7 +40,7 @@ const router = createBrowserRouter(createRoutesFromElements(
         <Route path="photos" element={<HostVanPhotos />} loader={async ({request}) => await requireAuth(request)} />
       </Route>
 
-      <Route path= "reviews" element={<Reviews/>} />
+      <Route path= "reviews" element={<Reviews/>} loader={async ({request}) => await requireAuth(request)}  />
     </Route>
     <Route path= "*" element={<NotFound/>} />
   </Route>
